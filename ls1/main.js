162a2407df9d34aa24a9c512e00ebad317e64408
tkohut1 <tar4ukkogut01@gmail.com>
2022-10-15 15:16:03 +0300

    Initial commit

---
+++ ls1/main.js
@@ -0,0 +1,25 @@
+// let a = 1;
+// let b = 258;
+// let c = -257;
+// let d = true;
+//
+// console.log(c);
+// // alert(a);
+// document.write(c);
+// console.log(typeof b);
+
+let a =prompt();
+// console.log(a)
+let b = prompt();
+// console.log(b)
+let c = prompt();
+let res = `${a} ${b} ${c}`
+console.log(res);
+
+let firstName = 'Taras';
+let midleName = 'Andr';
+let lastName = "Kogut";
+let person = `${firstName} ${midleName} ${lastName}`;
+document.write(person);
+
+
