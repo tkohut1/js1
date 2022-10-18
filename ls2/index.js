e327854334805b2d354c06b40a8b4fcf3a174609
tkohut1 <tar4ukkogut01@gmail.com>
2022-10-18 19:41:22 +0300

    2 і 3 домашка

---
+++ ls2/index.js
@@ -0,0 +1,56 @@
+// let mass = [1,'asd',true,false,346,"adsjhk",'sdf',765,23456,9876];
+// console.log(mass[0]);
+// console.log(mass[1]);
+// console.log(mass[2]);
+// console.log(mass[3]);
+// console.log(mass[4]);
+// console.log(mass[5]);
+// console.log(mass[6]);
+// console.log(mass[7]);
+// console.log(mass[8]);
+// console.log(mass[9]);
+
+// let book1 = {
+//     title: "qwe",
+//     pageCount: 258,
+//     genre: 'fantacy',
+//     avtor: [{name:'Edward', age:71}]
+// };
+//
+// let book2 = {
+//     title: "sdf",
+//     pageCount: 789,
+//     genre: "novel",
+//     avtor: [{name: 'Kate', age: 35}]
+// };
+//
+// let book3 = {
+//     title: "jhgf",
+//     pageCount: 129,
+//     genre: 'sdfasd',
+//     avtor: [{name: 'Mary', age:65}]
+// };
+// console.log(book1);
+
+let users = [
+    {name: "John", username: 'dfghb',password: 'sdfkhdf'},
+    {name: 'Steve', username: 'uyghbkj', password: 'hbkjn8532'},
+    {name:'Ghjbkng', username: 'sfydh', password: 26224524},
+    {name:'akjdfh', username: 'dfkhb', password: 'asdfs2824'},
+    {name: 'dfjkgdhjfgv', username: 'sfkjh', password: 'sfjg'},
+    {name: 'sfgjk', username: 'skjfgh', password: 'sfghj'},
+    {name: 'asdsad', username: 'sdjf', password: 655983335},
+    {name: 'dfga', username: 'sdfgsgf', password: 'sdgsdfgdfg'},
+    {name: 'sdfsdfsd', username: 'sdfcvq', password: 'dfkjbg'},
+    {name: 'sdfhkjd', username: 'sndvbhj', password: 'sdf55555'},
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
---
+++ ls3/main.js
@@ -0,0 +1,74 @@
+// let x = prompt('take number');
+// if (x != 0){
+//     document.write(true)
+// }else{
+//     document.write(false)
+// };
+
+// let time = prompt('0-59');
+// if (time >=0 && time <=15){
+//     document.write('First quater')
+// }else if (time >15 && time <=30){
+//     document.write('Second quater')
+// }else if (time >30 && time <= 45){
+//     document.write("third quater")
+// }else if (time >45 && time <= 59){
+//     document.write("Fourth quater")
+// }else{
+//     document.write("No that time!!!")
+// };
+
+
+// let day = prompt();
+// if (day >=1 && day <= 11){
+//     document.write("First decade");
+// }else if (day >11 && day <=21){
+//     document.write("Second decade");
+// }else if (day >21 && day <= 31){
+//     document.write("Third Decade");
+// }else{
+//     document.write("No day")
+// };
+
+// let day = prompt("Write the day.")
+// switch (day) {
+//     case "1":
+//         document.write("Monday");
+//         break;
+//     case "2":
+//         document.write('Tuesday');
+//         break;
+//     case "3":
+//         document.write("Wednesday");
+//         break;
+//     case "4":
+//         document.write("Thursday");
+//         break;
+//     case "5":
+//         document.write("Friday");
+//         break;
+//     case "6":
+//         document.write("Saturday");
+//         break;
+//     case "7":
+//         document.write("Sunday");
+//         break;
+//     default:
+//         document.write("No That Day!")
+//
+// }
+
+// let a = prompt();
+// let b = prompt();
+// if ( a > b){
+//     document.write(a);
+// }else if ( a < b){
+//     document.write(b);
+// }else if (a == b){
+//     document.write(a);
+// }else {
+//     document.write("???????");
+// };
+
+let x = prompt("wqe") || "default";
+console.log(x);
